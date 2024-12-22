0c140efa92b7f5f94b925930bb7eb3ae48b52ef7
a-korkin <a-korkin88@yandex.ru>
2024-12-22 17:25:05 +0300

    feat: running apps params

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/a-korkin/shop/configs"
-	"github.com/a-korkin/shop/internal/api"
-	// "github.com/a-korkin/shop/internal/rpc"
 	"github.com/a-korkin/shop/internal/adapters"
+	"github.com/a-korkin/shop/internal/api"
+	"github.com/a-korkin/shop/internal/rpc"
 	"log"
+	"os"
 )
 
-func main() {
+func help() {
+	fmt.Printf(`Params: 
+	-a, --api	run web api
+	-g, --grpc	run grpc server
+	-h, --help	show help
+`)
+}
+
+func runWebApi() {
 	dbConn, err := adapters.NewDBConnect(configs.GetDBConnection())
 	if err != nil {
 		log.Fatalf("failed to create connection to db: %s", err)
@@ -21,8 +30,36 @@ func main() {
 	}()
 	apiPort := configs.GetWebApiPort()
 	api.Run(fmt.Sprintf(":%s", apiPort), dbConn)
+}
 
-	// grpcPort := configs.GetGrpcPort()
-	// server := rpc.NewShopServer()
-	// server.Run(fmt.Sprintf(":%s", grpcPort))
+func runGrpcServer() {
+	grpcPort := configs.GetGrpcPort()
+	server := rpc.NewShopServer()
+	server.Run(fmt.Sprintf(":%s", grpcPort))
+}
+
+func main() {
+	if len(os.Args) == 1 || len(os.Args) > 2 {
+		fmt.Printf("You must choice which app to run. See help.\n")
+		help()
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "-a":
+		runWebApi()
+	case "--api":
+		runWebApi()
+	case "-g":
+		runGrpcServer()
+	case "--grpc":
+		runGrpcServer()
+	case "-h":
+		help()
+	case "--help":
+		help()
+	default:
+		fmt.Printf("Wrong argument: %s\n", os.Args[1])
+		help()
+		os.Exit(1)
+	}
 }
