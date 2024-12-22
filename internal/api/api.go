a9b34c2921eba5792315e745fd84c2be89a7f5d8
a-korkin <a-korkin88@yandex.ru>
2024-12-22 15:28:23 +0300

    feat: handler to api

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,24 +1,31 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/a-korkin/shop/internal/ports"
 	"log"
 	"net/http"
 	"strings"
 )
 
-type ShopHandler struct{}
+type ShopHandler struct {
+	Db ports.DbConnect
+}
 
 func (h *ShopHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	resource := strings.Split(strings.ToLower(r.RequestURI), "/")[1]
-	router(resource)
-	w.Write([]byte("hello from api"))
-}
 
-func router(resource string) {
 	switch resource {
 	case "items":
-
+		item, err := h.Db.GetItem(1)
+		if err != nil {
+			log.Fatalf("failed to get item: %s", err)
+		}
+		encoder := json.NewEncoder(w)
+		err = encoder.Encode(item)
+		if err != nil {
+			log.Fatalf("failed to encode item: %s", err)
+		}
 	}
 }
 
@@ -26,7 +33,9 @@ func Run(port string, dbConn ports.DbConnect) {
 	server := http.Server{
 		Addr: port,
 	}
-	handler := ShopHandler{}
+	handler := ShopHandler{
+		Db: dbConn,
+	}
 	http.Handle("/", &handler)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("failed to start web api: %s", err)
